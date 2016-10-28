633eed5fa1ed3222809498c9444e10a75fd495b4
Patrick Flynn <paflynn@google.com>
2016-10-28 16:27:08 -0400

    added stuff

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+mvn install
+mvn docker:build
+docker tag gct/spring-boot-simple gcr.io/positive-karma-844/simple-spring
+gcloud config set compute/zone us-central1-b
+gcloud config set project positive-karma-844
+gcloud docker push gcr.io/positive-karma-844/simple-spring
+gcloud container clusters create simple-spring
+gcloud container clusters get-credentials simple-spring
+kubectl run simple-spring --image=gcr.io/positive-karma-844/simple-spring --port=8080
+kubectl expose deployment simple-spring --type="LoadBalancer"
+
+## to cleanup
+## gcloud container clusters delete simple-spring
