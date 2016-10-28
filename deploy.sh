016e8e5b9e93aa5a0e82ba4af0d1ceca3534e045
Patrick Flynn <paflynn@google.com>
2016-10-28 17:43:17 -0400

    added cdb scope

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ docker tag gct/spring-boot-simple gcr.io/positive-karma-844/simple-spring
 gcloud config set compute/zone us-central1-b
 gcloud config set project positive-karma-844
 gcloud docker push gcr.io/positive-karma-844/simple-spring
-gcloud container clusters create simple-spring
+gcloud container clusters create simple-spring --scopes=https://www.googleapis.com/auth/cloud_debugger
 gcloud container clusters get-credentials simple-spring
 kubectl run simple-spring --image=gcr.io/positive-karma-844/simple-spring --port=8080
 kubectl expose deployment simple-spring --type="LoadBalancer"
