f33ac38a45741533b992fd4c1761cb411bf73f39
Patrick Flynn <paflynn@google.com>
2016-10-31 15:56:43 -0400

    update stuff

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 mvn install
 mvn docker:build
-docker tag gct/spring-boot-simple gcr.io/positive-karma-844/simple-spring
+docker tag gct/spring-boot-simple gcr.io/positive-karma-844/simple-spring:v3
 gcloud config set compute/zone us-central1-b
 gcloud config set project positive-karma-844
-gcloud docker push gcr.io/positive-karma-844/simple-spring
+gcloud docker push gcr.io/positive-karma-844/simple-spring:v3
 gcloud container clusters create simple-spring --scopes=https://www.googleapis.com/auth/cloud_debugger
 gcloud container clusters get-credentials simple-spring
 kubectl run simple-spring --image=gcr.io/positive-karma-844/simple-spring --port=8080
