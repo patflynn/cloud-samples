f8553d16e65ea4c1f3361e912d2dfffa62d71d7e
Patrick Flynn <paflynn@google.com>
2016-11-01 16:04:20 -0400

    templatize service name and version and bind gcr deployment to mvn deploy. also add the source context files to the jar root.

--- spring-boot-basic/src/main/java/hello/Application.java
+++ spring-boot-basic/src/main/java/hello/Application.java
@@ -1,5 +1,8 @@
 package hello;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
@@ -9,8 +12,19 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    InputStream in = Application.class.getClassLoader().getResourceAsStream("source-context.json");
     ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    if (in != null) {
+      System.out.println("Found source context.");
+      BufferedReader br = new BufferedReader(new InputStreamReader(in));
+      String line;
+      while ((line = br.readLine()) != null) {
+        System.out.println(line);
+      }
+    } else {
+      System.out.println("source context not found.");
+    }
 
     System.out.println("Let's inspect the beans provided by Spring Boot:");
 
