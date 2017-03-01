9581191d32f6bae71a2f653c0a9f49a674eaa466
Patrick Flynn <paflynn@google.com>
2017-03-01 09:25:20 -0500

    add state for no reason

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,13 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+  int i;
   static {
     System.out.println("my life");
   }
   @RequestMapping("/")
   public String index() {
-    int i = 0;
+    i++;
+    System.out.println("request #" + i);
     return "Greetings from Spring Boot!";
   }
-
 }
