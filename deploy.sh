0c9aa333e9722c3ba9589552bd319e8e1be07601
Patrick Flynn <big.pat@gmail.com>
2017-03-07 09:26:20 -0800

    rename k8s  service, update k8s cluster scope to include datastore, and rename artifact

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,18 @@ gcloud config set project positive-karma-844
 mvn deploy
 
 ## create the cluster if you haven't already
-gcloud container clusters create simple-spring --scopes=https://www.googleapis.com/auth/cloud_debugger
+gcloud container clusters create demo-app --scopes=https://www.googleapis.com/auth/cloud_debugger,https://www.googleapis.com/auth/datastore
 ## initialize your kubectl creds/context
 gcloud container clusters get-credentials simple-spring
 ## you'll probably have to do this for dumb reasons
 gcloud auth application-default login
 
 ## start the app
-kubectl run simple-spring --image=gcr.io/positive-karma-844/simple-spring --port=8080
+kubectl run demo-app --image=gcr.io/positive-karma-844/simple-spring --port=8080
 ## expose it to the world
-kubectl expose deployment simple-spring --type="LoadBalancer"
+kubectl expose deployment demo-app --type="LoadBalancer"
 ## checkout your public IP (may have to wait a few minutes)
-kubectl get service simple-spring
+kubectl get service demo-app
 
 ## run a local proxy to the kubectl API. Find it here:http://localhost:8081/ui
 kubectl proxy
@@ -27,10 +27,10 @@ kubectl proxy
 ## There's some policy stuff I need to look at to make it do this automatically.  In the mean time
 ## as long as you've published a new version of the app you just have to edit the file to use the
 ## latest version tag, save, and then exit.  Check the UI to see it update like magic.
-kubectl edit deployment simple-spring
+kubectl edit deployment demo-app
 
 ## to cleanup (don't waste the earth's computer resources!(and money))
-gcloud container clusters delete simple-spring
+gcloud container clusters delete demo-app
 
 ## You also need to delete the load balancers!! they cost money too. (I used the UI)
 
