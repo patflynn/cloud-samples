dc92f6de75ef7d295021e32340cdbc12c90c1272
Patrick Flynn <paflynn@google.com>
2017-09-26 17:32:04 -0400

    added app-engine-standard support

--- src/main/java/com/google/cloud/tools/cuj/SpringBootWithCloudLibrariesApplication.java
+++ src/main/java/com/google/cloud/tools/cuj/SpringBootWithCloudLibrariesApplication.java
@@ -2,11 +2,18 @@ package com.google.cloud.tools.cuj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWithCloudLibrariesApplication {
+public class SpringBootWithCloudLibrariesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithCloudLibrariesApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWithCloudLibrariesApplication.class);
 	}
 }
